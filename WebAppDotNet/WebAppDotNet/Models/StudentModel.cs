23fa09d9d71f85f3754f442ad088cf512c5c4398
Suraj <surajkumarele.2019@gmail.com>
2024-03-15 18:18:03 +0530

    Setup studentModel

---
+++ WebAppDotNet/WebAppDotNet/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAppDotNet.Models
+{
+    public class StudentModel
+    {
+        public int rollNo { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Gender { get; set; }
+
+        public int Standard { get; set; }
+    }
+}
