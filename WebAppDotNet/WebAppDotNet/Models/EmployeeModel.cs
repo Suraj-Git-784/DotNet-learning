20ffcecd3be1d1dd4856e1b359e2b59ceb6aba42
Suraj <surajkumarele.2019@gmail.com>
2024-03-19 10:44:14 +0530

    Revert "Install bootstrap, Model Binding, Form Creation and validation"
    This reverts commit 9b54100589a0ff90e003cbdec8f2b35682bd7b13.

--- WebAppDotNet/WebAppDotNet/Models/EmployeeModel.cs
+++ WebAppDotNet/WebAppDotNet/Models/EmployeeModel.cs
@@ -1,35 +1,10 @@
-﻿using Newtonsoft.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
-
-namespace WebAppDotNet.Models
+﻿namespace WebAppDotNet.Models
 {
     public class EmployeeModel
     {
-        [Required(ErrorMessage = "Name cannot be empty")]
-        [StringLength(15, MinimumLength = 3, ErrorMessage = "asdasd")]
-        public  string Name { get; set; }
-        //Enum = Collection of constants
-        [Required(ErrorMessage = "Age is required")]
-        public int? Age { get; set; }
-        //public Gender Gender { get; set; }
-        [Required(ErrorMessage = "Email cannot be empty")]
-        [EmailAddress]
-        public required string Email { get; set; }
-        [Required(ErrorMessage = "Password is requried")]
-        [RegularExpression(@"(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$")]
-        public required string Password { get; set; }
-        //public int Salary { get; set; }
-        //public required string Description { get; set; }
+        public int EmpId { get; set; }
+        public required string EmpName { get; set; }
+        public required string EmpDesignation { get; set; }
+        public int EmpSalary { get; set; }
     }
-
-    public enum Gender
-    {
-        Male, Female
-    }
-
-    //public class EmployeeModel
-    //{
-    //    public required string Name { get; set; }
-    //    public int Age { get; set; }
-    //}
 }
