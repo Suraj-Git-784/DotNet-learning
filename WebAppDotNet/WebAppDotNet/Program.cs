9b54100589a0ff90e003cbdec8f2b35682bd7b13
Suraj <surajkumarele.2019@gmail.com>
2024-03-19 10:43:48 +0530

    Install bootstrap, Model Binding, Form Creation and validation

--- WebAppDotNet/WebAppDotNet/Program.cs
+++ WebAppDotNet/WebAppDotNet/Program.cs
@@ -12,9 +12,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+ 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(); // Must use to show files in browser
 
 app.UseRouting();
 
