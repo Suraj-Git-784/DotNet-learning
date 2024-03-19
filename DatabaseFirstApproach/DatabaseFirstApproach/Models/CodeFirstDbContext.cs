20a57d8c6b0403ea48f4eb1e0255f1e98c0daf26
Suraj <surajkumarele.2019@gmail.com>
2024-03-19 19:47:31 +0530

    DatabaseFirstApproach

---
+++ DatabaseFirstApproach/DatabaseFirstApproach/Models/CodeFirstDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirstApproach.Models;
+
+public partial class CodeFirstDbContext : DbContext
+{
+    public CodeFirstDbContext()
+    {
+    }
+
+    public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-EMLSJJQ\\SQLEXPRESS;Database=CodeFirstDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.Property(e => e.EmployeeEmail)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.EmployeeGender)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasDefaultValue("");
+            entity.Property(e => e.EmployeeName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
